3fb30b274312153432b829ea4ea3f1e9f9388b23
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-06 18:52:43 +0100

    Navbar component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Placeholder from './Placeholder';
+import Navbar from './Navbar';
 
 ReactDOM.render(
   <div>
+    <Navbar />
     <Placeholder />
   </div>, 
   

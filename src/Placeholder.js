091bb16f89cea2b7e801bb1f526aa8b62c9ce70c
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-07 12:21:46 +0100

    empty set svg

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import "./Placeholder.css"
-import emptyset from "./empty-set.png"
+import logo from "./empty-set.svg"
 
 class Placeholder extends Component {
   render() {
     return (
       <div className="Placeholder">
-      <img src={emptyset}/>
+        <object type="image/svg+xml" data={logo} alt="emptyset">
+          Your browser does not support SVG
+        </object>
         <p>
           Welcome to my website! Currently under construction, please don't
           look behind the curtain.

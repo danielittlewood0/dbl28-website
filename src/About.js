e4533336c50f172bad9f965c9e95bac4af085442
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-22 13:07:28 +0100

    more About tinkering

--- src/About.js
+++ src/About.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Placeholder from './Placeholder';
-import about from 'about.md'
+import about from './about.md';
+class About extends Component {
 
-function About(){
-  return (
-    <div>
-    {about}
+  parseMarkdown(){
+    var text = ""
+    const Http = new XMLHttpRequest();
+    const url='https://api.github.com/markdown/raw';
+    Http.open("POST", url);
+    Http.send(text);
+    Http.onreadystatechange=(e)=>{
+        if (Http.readyState == 4 && Http.status == 200) {
+          this.setState({content: Http.responseText})
+          var readme = document.getElementById('readme')
+          readme.innerHTML = Http.responseText
+      }
+    }
+  }
+
+  fetchContent(){
+    const Local = new XMLHttpRequest();
+    Local.open("GET",about);
+    Local.send();
+    Local.onreadystatechange = function() {
+      if (Local.readyState== 4 && Local.status == 200) {
+        alert(this.parseMarkdown)
+        this.parseMarkdown(Local.responseText);
+      }
+    }
+  }
+
+  constructor(){
+    super()
+    this.state = {
+      content: "Parsing markdown..."
+    }
+    this.parseMarkdown = this.parseMarkdown.bind(this);
+    this.fetchContent = this.fetchContent.bind(this);
+    this.fetchContent()
+  }
+
+  render(){
+    return (
+    <div id="readme">
+      {this.state.content}
     </div>
-  )
+  )}
 }
 
 export default About;
--- src/about.md
+++ src/about.md
@@ -4,5 +4,3 @@ This website is a tester project.
 I wanted to learn some Javascript, and some React, 
 and this seemed like a natural way to do both 
 while also making something useful and nice to look at. 
-      
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).

cd206d922dc04aaa766e1f5e4ca8d97ebc3e467d
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-25 17:43:33 +0100

    Styling Life component

--- src/Life.js
+++ src/Life.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Placeholder from './Placeholder';
+import LifeBoard from './js-life/src/LifeBoard.js';
+import './Life.css';
 
 function Life(){
   return (
     <div>
-      <h2> I will contain a Game of Life implementation, soon.</h2>
-      <Placeholder/>
+      <h1> Conway's Game of Life.</h1>
+      <div className="life-container">
+        <LifeBoard width={5} height={5}/>
+      </div>
     </div>
   )
 }

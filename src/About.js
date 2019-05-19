71ed3e636847b48d3fb95eb921db7d1b6012f02c
Daniel Littlewood <danielittlewood@gmail.com>
2019-05-19 23:39:20 +0100

    About markdown bug fixed

--- src/About.js
+++ src/About.js
@@ -2,34 +2,6 @@ import React,{Component} from 'react';
 import Placeholder from './Placeholder';
 import about from './about.md';
 class About extends Component {
-
-  parseMarkdown(){
-    var text = ""
-    const Http = new XMLHttpRequest();
-    const url='https://api.github.com/markdown/raw';
-    Http.open("POST", url);
-    Http.send(text);
-    Http.onreadystatechange=(e)=>{
-        if (Http.readyState == 4 && Http.status == 200) {
-          this.setState({content: Http.responseText})
-          var readme = document.getElementById('readme')
-          readme.innerHTML = Http.responseText
-      }
-    }
-  }
-
-  fetchContent(){
-    const Local = new XMLHttpRequest();
-    Local.open("GET",about);
-    Local.send();
-    Local.onreadystatechange = function() {
-      if (Local.readyState== 4 && Local.status == 200) {
-        alert(this.parseMarkdown)
-        this.parseMarkdown(Local.responseText);
-      }
-    }
-  }
-
   constructor(){
     super()
     this.state = {
@@ -38,6 +10,34 @@ class About extends Component {
     this.parseMarkdown = this.parseMarkdown.bind(this);
     this.fetchContent = this.fetchContent.bind(this);
     this.fetchContent()
+  }
+
+
+  parseMarkdown(text){
+    const Http = new XMLHttpRequest();
+    const url='https://api.github.com/markdown/raw';
+    const This = this;
+    Http.open("POST", url);
+    Http.send(text);
+    Http.onreadystatechange=(e)=>{
+        if (Http.readyState == 4 && Http.status == 200) {
+          This.setState({content: Http.responseText})
+          var readme = document.getElementById('readme')
+          readme.innerHTML = Http.responseText
+      }
+    }
+  }
+
+  fetchContent(){
+    const Local = new XMLHttpRequest();
+    const This = this;
+    Local.open("GET",about);
+    Local.send();
+    Local.onreadystatechange = function() {
+      if (Local.readyState== 4 && Local.status == 200) {
+        This.parseMarkdown(Local.responseText);
+      }
+    }
   }
 
   render(){

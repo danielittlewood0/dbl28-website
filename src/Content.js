fba26b0861a46d59edb5dcdcddf403cabc40c4c8
Daniel Littlewood <danielittlewood@gmail.com>
2019-07-10 00:30:00 +0100

    content component appears not to do anything...?

--- src/Content.js
+++ src/Content.js
@@ -8,16 +8,12 @@ import Interests from './Interests.js';
 import Contact from './Contact.js';
 
 class Content extends Component{
-
   render(props){
-    if (this.props.page == "About"){ return ( <About/> ) }
-    else if (this.props.page == "Maths"){ return ( <Maths/> ) }
-    else if (this.props.page == "Life"){ return ( <Life/> ) }
-    else if (this.props.page == "Interests"){ return ( <Interests/> ) }
-    else if (this.props.page == "Contact"){ return ( <Contact/> ) }
-    else if (this.props.page == "Work"){ return ( <Work/> ) }
-    else if (this.props.page != null){ return(<div> Sorry, I don't know the page {this.props.page}! </div>) }
-    else { return (<div/>)}
+    return (
+      <div id="content">
+        {"hello"}
+      </div>
+    )
   }
 }
 
